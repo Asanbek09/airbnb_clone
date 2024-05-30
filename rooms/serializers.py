c153118457c2dffcbf0d76a6b89fd6cbba83cc18
Asanbek09 <samuidnua@gmail.com>
2024-05-30 14:05:20 +0600

    new steps

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -4,6 +4,7 @@ from users.serializers import RelatedUserSerializer
 
 class RoomSerializer(serializers.ModelSerializer):
     user = RelatedUserSerializer()
+    is_fav = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -20,3 +21,11 @@ class RoomSerializer(serializers.ModelSerializer):
             if check_in == check_out:
                 raise serializers.ValidationError("Not Enough time between changes")
         return data
+
+    def get_is_fav(self, obj):
+        request = self.context.get("request")
+        if request:
+            user = request.user
+            if user.is_authenticated:
+                return obj in user.favs.all()
+        return False
