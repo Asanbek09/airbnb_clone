8ba8feaacb3a260604fc4e4b024808748b231d53
Asanbek09 <samuidnua@gmail.com>
2024-05-31 11:27:59 +0600

    users/views.py изменения завершены

--- users/views.py
+++ users/views.py
@@ -6,7 +6,6 @@ from rest_framework.decorators import action, permission_classes
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .serializers import UserSerializer
 from .permissions import IsSelf
